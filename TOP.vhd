fd3d8f49ce64c26c7534e226047a0971f5a5f232
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-09-10 14:21:26 +0300

    Update TOP.vhd
    Instance

--- TOP.vhd
+++ TOP.vhd
@@ -102,7 +102,7 @@ begin
             STAT_REGS           =>  STAT_REGS                 
         );
     
-    U3: entity work.UART_TRX_DBG
+    U3_DEBUG: entity work.UART_TRX_DBG
         port map 
         (
         
@@ -123,68 +123,70 @@ begin
             TX_BUF_FULL_DBG     =>    STAT_REGS.ext_interface_tx_buf_full_dbg
         );
         
-    U4: entity work.UART_TRX
-            port map 
-            (
-            
-            CLK                 =>    CLK_100,
-            RST                 =>    i_rst,
-            
-            CLK_DIV_BAUD_UART   =>    CTRL_REGS.ext_interface_clk_div_baud_uart,
-            
-            RX_IN_UART          =>    UART_RX_IN,
-            TX_OUT_UART         =>    UART_TX_OUT,
-      
-            RX_BUF_EMPTY_UART   =>    STAT_REGS.ext_interface_rx_buf_empty_uart,
-            RX_BUF_DATA_UART    =>    STAT_REGS.ext_interface_rx_buf_data_uart,
-            RX_BUF_RDEN_UART    =>    CTRL_REGS.ext_interface_rx_buf_rden_uart,
-      
-            TX_BUF_DATA_UART    =>    CTRL_REGS.ext_interface_tx_buf_data_uart,
-            TX_BUF_WREN_UART    =>    CTRL_REGS.ext_interface_tx_buf_wren_uart,
-            TX_BUF_FULL_UART    =>    STAT_REGS.ext_interface_tx_buf_full_uart
-            );
-
-    U5: entity work.UART_TRX_RS232
-            port map 
-            (
-            
-            CLK                 =>    CLK_100,
-            RST                 =>    i_rst,
-            
-            CLK_DIV_BAUD_RS232  =>    CTRL_REGS.ext_interface_clk_div_baud_rs232,
-            
-            RX_IN_RS232         =>    UART_RX_IN_RS232,
-            TX_OUT_RS232        =>    UART_TX_OUT_RS232,
-      
-            RX_BUF_EMPTY_RS232  =>    STAT_REGS.ext_interface_rx_buf_empty_rs232,
-            RX_BUF_DATA_RS232   =>    STAT_REGS.ext_interface_rx_buf_data_rs232,
-            RX_BUF_RDEN_RS232   =>    CTRL_REGS.ext_interface_rx_buf_rden_rs232,
-      
-            TX_BUF_DATA_RS232   =>    CTRL_REGS.ext_interface_tx_buf_data_rs232,
-            TX_BUF_WREN_RS232   =>    CTRL_REGS.ext_interface_tx_buf_wren_rs232,
-            TX_BUF_FULL_RS232   =>    STAT_REGS.ext_interface_tx_buf_full_rs232
-            );
-            
-    U6: entity work.UART_TRX_RS422
-                    port map 
-                    (
-                    
-            CLK                 =>    CLK_100,
-            RST                 =>    i_rst,
-            
-            CLK_DIV_BAUD_RS422  =>    CTRL_REGS.ext_interface_clk_div_baud_rs422,
-            
-            RX_IN_RS422         =>    UART_RX_IN_RS422,
-            TX_OUT_RS422        =>    UART_TX_OUT_RS422,
-            
-            RX_BUF_EMPTY_RS422  =>    STAT_REGS.ext_interface_rx_buf_empty_rs422,
-            RX_BUF_DATA_RS422   =>    STAT_REGS.ext_interface_rx_buf_data_rs422,
-            RX_BUF_RDEN_RS422   =>    CTRL_REGS.ext_interface_rx_buf_rden_rs422,
-            
-            TX_BUF_DATA_RS422   =>    CTRL_REGS.ext_interface_tx_buf_data_rs422,
-            TX_BUF_WREN_RS422   =>    CTRL_REGS.ext_interface_tx_buf_wren_rs422,
-            TX_BUF_FULL_RS422   =>    STAT_REGS.ext_interface_tx_buf_full_rs422
-                    );
+     U4_RS422: entity work.UART_TRX_DBG
+           port map 
+           (
+           
+               CLK                 =>    CLK_100,
+               RST                 =>    i_rst,
+               
+               CLK_DIV_BAUD_DBG    =>    CTRL_REGS.ext_interface_clk_div_baud_rs422, 
+               
+               RX_IN_DBG           =>    UART_RX_IN_RS422,                           
+               TX_OUT_DBG          =>    UART_TX_OUT_RS422,                          
+        
+               RX_BUF_EMPTY_DBG    =>    STAT_REGS.ext_interface_rx_buf_empty_rs422, 
+               RX_BUF_DATA_DBG     =>    STAT_REGS.ext_interface_rx_buf_data_rs422,  
+               RX_BUF_RDEN_DBG     =>    CTRL_REGS.ext_interface_rx_buf_rden_rs422,  
+        
+               TX_BUF_DATA_DBG     =>    CTRL_REGS.ext_interface_tx_buf_data_rs422,  
+               TX_BUF_WREN_DBG     =>    CTRL_REGS.ext_interface_tx_buf_wren_rs422,  
+               TX_BUF_FULL_DBG     =>    STAT_REGS.ext_interface_tx_buf_full_rs422   
+           );
+               
+       U5_RS232: entity work.UART_TRX_DBG
+                  port map 
+                  (
+                  
+                      CLK                 =>    CLK_100,
+                      RST                 =>    i_rst,
+                      
+                      CLK_DIV_BAUD_DBG    =>    CTRL_REGS.ext_interface_clk_div_baud_rs232,
+                      
+                      RX_IN_DBG           =>    UART_RX_IN_RS232,                          
+                      TX_OUT_DBG          =>    UART_TX_OUT_RS232,                         
+               
+                      RX_BUF_EMPTY_DBG    =>    STAT_REGS.ext_interface_rx_buf_empty_rs232,
+                      RX_BUF_DATA_DBG     =>    STAT_REGS.ext_interface_rx_buf_data_rs232, 
+                      RX_BUF_RDEN_DBG     =>    CTRL_REGS.ext_interface_rx_buf_rden_rs232, 
+               
+                      TX_BUF_DATA_DBG     =>    CTRL_REGS.ext_interface_tx_buf_data_rs232, 
+                      TX_BUF_WREN_DBG     =>    CTRL_REGS.ext_interface_tx_buf_wren_rs232, 
+                      TX_BUF_FULL_DBG     =>    STAT_REGS.ext_interface_tx_buf_full_rs232  
+                  );
+                      
+         U6_UART: entity work.UART_TRX_DBG
+                     port map 
+                     (
+                     
+                         CLK                 =>    CLK_100,
+                         RST                 =>    i_rst,
+                         
+                         CLK_DIV_BAUD_DBG    =>    CTRL_REGS.ext_interface_clk_div_baud_uart,
+                         
+                         RX_IN_DBG           =>    UART_RX_IN,                               
+                         TX_OUT_DBG          =>    UART_TX_OUT,                              
+                  
+                         RX_BUF_EMPTY_DBG    =>    STAT_REGS.ext_interface_rx_buf_empty_uart,
+                         RX_BUF_DATA_DBG     =>    STAT_REGS.ext_interface_rx_buf_data_uart, 
+                         RX_BUF_RDEN_DBG     =>    CTRL_REGS.ext_interface_rx_buf_rden_uart, 
+                  
+                         TX_BUF_DATA_DBG     =>    CTRL_REGS.ext_interface_tx_buf_data_uart, 
+                         TX_BUF_WREN_DBG     =>    CTRL_REGS.ext_interface_tx_buf_wren_uart, 
+                         TX_BUF_FULL_DBG     =>    STAT_REGS.ext_interface_tx_buf_full_uart  
+                     );
+        
+    
     
     
 end Behavioral;
